e8d08b019b7073099e9c7bc8a881e1fdc418319d
codeman_start <2477373558@qq.com>
2022-09-20 18:18:45 +0800

    初始的文件

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+<!--
+ * @Date: 2022-09-20 18:14:17
+ * @LastEditors: 冯文魁
+ * @LastEditTime: 2022-09-20 18:16:28
+ * @FilePath: \demo\src\App.vue
+-->
+
+<template></template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
