34137ddd302db48a21698db0642b8276cee3b3a5
codeman_start <2477373558@qq.com>
2022-09-22 21:58:58 +0800

    具名插槽

---
+++ 具名插槽src/Vue过过度实现动画.vue
@@ -0,0 +1,44 @@
+<!--
+ * @Date: 2022-09-20 18:14:17
+ * @LastEditors: 冯文魁
+ * @LastEditTime: 2022-09-22 15:51:38
+ * @FilePath: \demo\src\App.vue
+-->
+
+<template>
+  <div>
+    <transition name=" remove" appear>
+      <h1 v-show="show">俺来也</h1>
+    </transition>
+    <button @click="show = !show">显示、隐藏</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: true,
+    };
+  },
+};
+</script>
+
+<style>
+h1 {
+  height: 100px;
+  background-color: skyblue;
+}
+.remove-enter,
+.remove-leave-to {
+  transform: translateX(-100%);
+}
+.remove-enter-to,
+.remove-leave {
+  transform: translateX(0px);
+}
+.remove-enter-active,
+.remove-leave-active {
+  transition: 0.5s linear;
+}
+</style>
