d13b6cff4532b8ea5f0a0014f6ca0cf3da0555bc
codeman_start <2477373558@qq.com>
2022-09-25 19:09:33 +0800

    编程式导航

---
+++ router路由/路由编程式导航src/views/Find.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Date: 2022-09-25 10:51:11
+ * @LastEditors: 冯文魁
+ * @LastEditTime: 2022-09-25 11:32:40
+ * @FilePath: \demo\src\views\Find.vue
+-->
+<template>
+  <div>
+    <p>77</p>
+    <p>88</p>
+    <p>99</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ router路由/路由编程式导航src/views/Part.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>11</p>
+    <p>22</p>
+    <p>33</p>
+    <p>人名--{{ $route.query.name }}</p>
+    <p>人名--{{ $route.params.username }}</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
